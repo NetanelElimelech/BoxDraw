a317b64f7b2db9cabfd9cd1317b4a546d1366f8a
NetanelElimelech <44609836+NetanelElimelech@users.noreply.github.com>
2019-05-19 00:39:16 +0300

    GUI built

---
+++ BoxDraw/Box.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxDraw
+{
+    class Box
+    {
+        float Height { get; set; }
+        float Width { get; set; }
+        float Depth { get; set; }
+        float TopWidth { get; set; }
+        float TopLength { get; set; }
+        float GlassThickness { get; set; }
+    }
+}
